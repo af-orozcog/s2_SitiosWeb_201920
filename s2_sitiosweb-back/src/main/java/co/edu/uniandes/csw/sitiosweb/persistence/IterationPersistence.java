41cd80baa379abeb598667baaf380d993e74e726
Andres Orozco <af.orozcog@uniandes.edu.co>
2019-08-20 10:46:25 -0500

    se agrega la clase Iteration Persistance

---
+++ s2_sitiosweb-back/src/main/java/co/edu/uniandes/csw/sitiosweb/persistence/IterationPersistence.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.sitiosweb.persistence;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Andrés Felipe Orozco Gonzalez
+ */
+@Stateless
+public class IterationPersistence {
+    public IterationPersistence create (IterationPersistence iteration){
+        throw new java.lang.UnsupportedOperationException("Not supported yet.");
+    }
+}
