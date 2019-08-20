900ec4d6b3a8f8bafdef0515bec0a59789d9d180
Daniel del Castillo <d.delcastillo@uniandes.edu.co>
2019-08-20 10:55:13 -0500

    Creación RequestPersistence + RequestPersistenceTest

---
+++ s2_sitiosweb-back/src/main/java/co/edu/uniandes/csw/sitiosweb/persistence/RequestPersistence.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.sitiosweb.persistence;
+
+import co.edu.uniandes.csw.sitiosweb.entities.RequestEntity;
+
+/**
+ *
+ * @author Daniel del Castillo
+ */
+public class RequestPersistence 
+{
+    public RequestEntity create(RequestEntity request)
+    {
+        throw new java.lang.UnsupportedOperationException("Not supported yet.");
+    }
+}
