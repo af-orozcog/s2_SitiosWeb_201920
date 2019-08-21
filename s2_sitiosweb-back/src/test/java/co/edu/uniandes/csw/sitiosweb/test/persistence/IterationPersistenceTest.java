529386e280b4640eb3c650cf16dfbcbbb06664f8
Andres Orozco <af.orozcog@uniandes.edu.co>
2019-08-20 22:20:36 -0500

    se modifica la clase IterationPersistenceTest

--- s2_sitiosweb-back/src/test/java/co/edu/uniandes/csw/sitiosweb/test/persistence/IterationPersistenceTest.java
+++ s2_sitiosweb-back/src/test/java/co/edu/uniandes/csw/sitiosweb/test/persistence/IterationPersistenceTest.java
@@ -5,8 +5,19 @@
  */
 package co.edu.uniandes.csw.sitiosweb.test.persistence;
 
+import co.edu.uniandes.csw.sitiosweb.entities.IterationEntity;
+import co.edu.uniandes.csw.sitiosweb.persistence.IterationPersistence;
+import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import co.edu.uniandes.csw.sitiosweb.persistence.IterationPersistence;
+import javax.inject.Inject;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
 
 /**
  *
@@ -14,5 +25,24 @@ import org.junit.runner.RunWith;
  */
 @RunWith(Arquillian.class)
 public class IterationPersistenceTest {
+   
+    @Deployment
+    public static JavaArchive createDeploymet(){
+        return ShrinkWrap.create(JavaArchive.class)
+                .addClass(IterationEntity.class)
+                .addClass(IterationPersistence.class)
+                .addAsManifestResource("META-INF/persistance.xml","persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml","beans.xml");
+    }
+    @Inject
+    IterationPersistence ep;
     
+    
+    @Test
+    public void createTest(){
+        PodamFactory factory = new PodamFactoryImpl();
+        IterationEntity iteration = factory.manufacturePojo(IterationEntity.class);
+        IterationEntity result = ep.create(iteration);
+        Assert.assertNotNull(result);
+    }
 }
