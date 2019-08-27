3cdec9be948169cf848e9bfe0a0471a0ddc1f13d
Andres Orozco <af.orozcog@uniandes.edu.co>
2019-08-26 21:17:06 -0500

    se terminan de agregar los métodos del CRUD

--- s2_sitiosweb-back/src/main/java/co/edu/uniandes/csw/sitiosweb/persistence/IterationPersistence.java
+++ s2_sitiosweb-back/src/main/java/co/edu/uniandes/csw/sitiosweb/persistence/IterationPersistence.java
@@ -43,9 +43,9 @@ public class IterationPersistence {
      */
     public List<IterationEntity> findAll() {
         LOGGER.log(Level.INFO, "Consultando todas las iteraciones");
-        // Se crea un query para buscar todas las authores en la base de datos.
+        // Se crea un query para buscar todas las iteraciones en la base de datos.
         TypedQuery query = em.createQuery("select u from IterationEntity u",IterationEntity.class);
-        // Note que en el query se hace uso del método getResultList() que obtiene una lista de authores.
+        // Note que en el query se hace uso del método getResultList() que obtiene una lista de las iteraciones.
         return query.getResultList();
     }
     /**
@@ -63,5 +63,37 @@ public class IterationPersistence {
         return em.find(IterationEntity.class, iterationId);
     }
     
+    /**
+     * Actualiza una iteracion.
+     *
+     * @param iterationEntity: la iteracion que viene con los nuevos cambios. Por
+     * ejemplo el nombre pudo cambiar. En ese caso, se haria uso del método
+     * update.
+     * @return una iteracion con los cambios aplicados.
+     */
+    public IterationEntity update(IterationEntity iterationEntity) {
+        LOGGER.log(Level.INFO, "Actualizando la iteracion con id={0}", iterationEntity.getId());
+        /* Note que hacemos uso de un método propio del EntityManager llamado merge() que recibe como argumento
+        la iteracion con los cambios, esto es similar a 
+        "UPDATE table_name SET column1 = value1, column2 = value2, ... WHERE condition;" en SQL.
+         */
+        return em.merge(iterationEntity);
+    }
     
+    /**
+     * Borra una iteracion de la base de datos recibiendo como argumento el id de la
+     * iteracion
+     *
+     * @param iterationId: id correspondiente a la iteracion a borrar.
+     */
+    public void delete(Long iterationId) {
+
+        LOGGER.log(Level.INFO, "Borrando la iteracion con id={0}", iterationId);
+        // Se hace uso de mismo método que esta explicado en public IterationEntity find(Long id) para obtener la iteracion a borrar.
+        IterationEntity iterationEntity = em.find(IterationEntity.class, iterationId);
+        /* Note que una vez obtenido el objeto desde la base de datos llamado "entity", volvemos hacer uso de un método propio del
+        EntityManager para eliminar de la base de datos el objeto que encontramos y queremos borrar.
+        Es similar a "delete from IterationEntity where id=id;" - "DELETE FROM table_name WHERE condition;" en SQL.*/
+        em.remove(iterationEntity);
+    }
 }
