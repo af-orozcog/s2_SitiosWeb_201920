eef05562d7c8cd96311225be405c42b32faacd23
Daniel Galindo Ruiz <d.galindo@uniandes.edu.co>
2019-08-26 22:25:42 -0500

    Test de ProjectPersistence terminado

--- s2_sitiosweb-back/src/test/java/co/edu/uniandes/csw/sitiosweb/test/persistence/ProjectPersistenceTest.java
+++ s2_sitiosweb-back/src/test/java/co/edu/uniandes/csw/sitiosweb/test/persistence/ProjectPersistenceTest.java
@@ -7,14 +7,18 @@ package co.edu.uniandes.csw.sitiosweb.test.persistence;
 
 import co.edu.uniandes.csw.sitiosweb.entities.ProjectEntity;
 import co.edu.uniandes.csw.sitiosweb.persistence.ProjectPersistence;
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
 import junit.framework.Assert;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import uk.co.jemos.podam.api.PodamFactory;
@@ -42,6 +46,51 @@ public class ProjectPersistenceTest {
    @PersistenceContext
    EntityManager em;
    
+   @Inject
+   UserTransaction utx;
+   
+   private List<ProjectEntity> data = new ArrayList<ProjectEntity>();
+   
+   /**
+    * Configuracion de la prueba
+    */
+   @Before
+   public void configTest(){
+       try{
+           utx.begin();
+           em.joinTransaction();
+           clearData();
+           insertData();
+           utx.commit();
+       } 
+       catch(Exception e){
+           e.printStackTrace();
+           try{
+               utx.rollback();
+           }
+           catch(Exception e1){
+               e1.printStackTrace();
+           }
+       }
+   }
+   
+   /**
+    * Limpia tablas de la prueba
+    */
+   private void clearData(){
+       em.createQuery("delete from ProjectEntity").executeUpdate();
+   }
+   
+   private void insertData(){
+       PodamFactory factory = new PodamFactoryImpl();
+       for(int i = 0; i < 3; i++){
+           
+           ProjectEntity project = factory.manufacturePojo(ProjectEntity.class);
+           em.persist(project);
+           data.add(project);
+       }
+   }
+   
    @Test
    public void createTest(){
        PodamFactory factory = new PodamFactoryImpl();
@@ -52,6 +101,54 @@ public class ProjectPersistenceTest {
        ProjectEntity entity = em.find(ProjectEntity.class, result.getId());
 
         Assert.assertEquals(project.getCompany(),entity.getCompany());
+        Assert.assertEquals(project.getInternalProject(), entity.getInternalProject());
 
+   }
+   
+   @Test
+   public void getProjectsTest(){
+       List<ProjectEntity> list = pp.findAll();
+       Assert.assertEquals(data.size(), list.size());
+       
+       for(ProjectEntity ent:list){
+           boolean found = false;
+           
+           for(ProjectEntity entity:data){
+               
+               if(ent.getId().equals(entity.getId())){
+                   found = true;
+               }
+           }
+           Assert.assertTrue(found);
+       }
+   }
+   
+   @Test
+   public void getProjectEntiyTest(){
+       ProjectEntity entity = data.get(0);
+       ProjectEntity newEntity = pp.find(entity.getId());
+       Assert.assertNotNull(newEntity);
+       Assert.assertEquals(entity.getCompany(), newEntity.getCompany());
+   }
+   
+   @Test
+   public void deleteProjectTest(){
+       ProjectEntity entity = data.get(0);
+       pp.delete(entity.getId());
+       ProjectEntity deleted = em.find(ProjectEntity.class, entity.getId());
+       Assert.assertNull(deleted);
+   }
+   
+   @Test
+   public void updateProjectTest(){
+       ProjectEntity entity = data.get(0);
+       PodamFactory factory = new PodamFactoryImpl();
+       ProjectEntity newEntity = factory.manufacturePojo(ProjectEntity.class);
+       
+       newEntity.setId(entity.getId());
+       pp.update(newEntity);
+       
+       ProjectEntity resp = em.find(ProjectEntity.class, entity.getId());
+       Assert.assertEquals(newEntity.getCompany(), resp.getCompany());
    }
 }
