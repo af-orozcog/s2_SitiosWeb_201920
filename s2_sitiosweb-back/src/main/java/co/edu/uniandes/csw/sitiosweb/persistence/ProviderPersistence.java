7f770dc6b99507480bf27cf0b8aaf7af725c7bec
Andres Martinez <a.martinez@uniandes.edu.co>
2019-08-26 21:42:46 -0500

    ProviderEntity CRUD

--- s2_sitiosweb-back/src/main/java/co/edu/uniandes/csw/sitiosweb/persistence/ProviderPersistence.java
+++ s2_sitiosweb-back/src/main/java/co/edu/uniandes/csw/sitiosweb/persistence/ProviderPersistence.java
@@ -7,9 +7,13 @@ package co.edu.uniandes.csw.sitiosweb.persistence;
 
 import co.edu.uniandes.csw.sitiosweb.entities.ProviderEntity;
 import co.edu.uniandes.csw.sitiosweb.entities.RequestEntity;
+import static com.sun.xml.internal.ws.spi.db.BindingContextFactory.LOGGER;
+import java.util.List;
+import java.util.logging.Level;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 /**
  * @author Andres Martinez Silva 
@@ -22,9 +26,36 @@ public class ProviderPersistence {
 
     
     public ProviderEntity create (ProviderEntity provider){
-        //throw new java.lang.UnsupportedOperationException("Not supported yet.");
+        LOGGER.log(Level.INFO, "Creando un proeevedor nuevo");
         em.persist(provider);
-
+        LOGGER.log(Level.INFO, "Proveedor creado");
         return provider;
+    }
+    
+    
+    public List<ProviderEntity> findAll() {
+        LOGGER.log(Level.INFO, "Consultando todos los proveedores");
+
+        TypedQuery query = em.createQuery("select u from ProviderEntity u", ProviderEntity.class);
+       
+        return query.getResultList();
+    }
+    
+        public ProviderEntity find(Long authorsId) {
+        LOGGER.log(Level.INFO, "Consultando el proveedor con id={0}", authorsId);
+
+        return em.find(ProviderEntity.class, authorsId);
+    }
+        
+        public ProviderEntity update(ProviderEntity authorEntity) {
+        LOGGER.log(Level.INFO, "Actualizando el proveedor con id={0}", authorEntity.getId());
+        return em.merge(authorEntity);
+    }
+        
+            public void delete(Long authorsId) {
+
+        LOGGER.log(Level.INFO, "Borrando el proveedor con id={0}", authorsId);
+        ProviderEntity authorEntity = em.find(ProviderEntity.class, authorsId);
+        em.remove(authorEntity);
     }
 }
