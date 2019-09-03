2da781523cfccaa5b57ac333ce0630159eda5a74
nf.abondano <nf.abondano@uniandes.edu.co>
2019-09-03 10:23:27 -0500

    Implementación primer método

--- s2_sitiosweb-back/src/main/java/co/edu/uniandes/csw/sitiosweb/ejb/UserLogic.java
+++ s2_sitiosweb-back/src/main/java/co/edu/uniandes/csw/sitiosweb/ejb/UserLogic.java
@@ -5,10 +5,31 @@
  */
 package co.edu.uniandes.csw.sitiosweb.ejb;
 
+import co.edu.uniandes.csw.sitiosweb.entities.UserEntity;
+import co.edu.uniandes.csw.sitiosweb.exceptions.BusinessLogicException;
+import co.edu.uniandes.csw.sitiosweb.persistence.UserPersistence;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
 /**
  *
- * @author Estudiante
+ * @author Nicolás Abondano nf.abondano 201812467
  */
+
+@Stateless
 public class UserLogic {
     
+    @Inject
+    private UserPersistence persistence;
+    
+    public UserEntity createUser( UserEntity user) throws BusinessLogicException{
+        
+        if(user.getLogin() == null )
+            throw new BusinessLogicException( "El login del usuario está vacío" );
+        if(user.getEmail() == null )
+            throw new BusinessLogicException( "El email del usuario está vacío" );
+        
+        user = persistence.create(user);
+        return user;
+    }
 }
