7e43c45b8afbf569885f3da95616a6f943898b70
Sebastian Santos Bautista <s.santosb@uniandes.edu.co>
2019-09-10 10:46:51 -0500

    Tests HardwareLogic listos

--- s2_sitiosweb-back/src/main/java/co/edu/uniandes/csw/sitiosweb/ejb/HardwareLogic.java
+++ s2_sitiosweb-back/src/main/java/co/edu/uniandes/csw/sitiosweb/ejb/HardwareLogic.java
@@ -8,6 +8,8 @@ package co.edu.uniandes.csw.sitiosweb.ejb;
 import co.edu.uniandes.csw.sitiosweb.entities.HardwareEntity;
 import co.edu.uniandes.csw.sitiosweb.exceptions.BusinessLogicException;
 import co.edu.uniandes.csw.sitiosweb.persistence.HardwarePersistence;
+import java.util.List;
+import java.util.logging.Level;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
@@ -18,15 +20,64 @@ import javax.inject.Inject;
 @Stateless
 public class HardwareLogic {
     @Inject
-    private HardwarePersistence persitence;
+    private HardwarePersistence persistence;
     
     public HardwareEntity createHardware(HardwareEntity hardware) throws BusinessLogicException{
         if(hardware.getIp()==null){
             throw new BusinessLogicException("El ip del hardware esta vacio");
         }
         
-        hardware = persitence.create(hardware);
+        if(hardware.getCores()==0){
+            throw new BusinessLogicException("Los nucleos del harware no son correctos");
+        }
+        
+        if(hardware.getRam()==0){
+            throw new BusinessLogicException("La RAM del hardware no es valida");
+        }
+        
+        if(hardware.getCpu()==null){
+            throw new BusinessLogicException("El cpu del hardware esta vacio");
+        }
+        
+        if(hardware.getPlataforma()==null){
+            throw new BusinessLogicException("La plataforma del hardware esta vacia");
+        }
+        
+        hardware = persistence.create(hardware);
         return hardware;
+    }
+    
+    /**
+     * Devuelve todos los hardwares que hay en la base de datos.
+     *
+     * @return Lista de entidades de tipo hardware.
+     */
+    public List<HardwareEntity> getHardwares() {
+        List<HardwareEntity> hardwares = persistence.findAll();
+        return hardwares;
+    }
+
+    /**
+     * Busca un hardware por ID
+     *
+     * @param hardwareId El id del hardware a buscar
+     * @return El hardware encontrado, null si no lo encuentra.
+     */
+    public HardwareEntity getHardware(Long hardwareId) {
+        HardwareEntity hardwareEntity = persistence.find(hardwareId);
+        return hardwareEntity;
+    }
+
+    /**
+     * Actualizar un hardware por ID
+     *
+     * @param hardwareId El ID del libro a actualizar
+     * @param hardwareEntity La entidad del libro con los cambios deseados
+     * @return La entidad del hardware luego de actualizarla
+     */
+    public HardwareEntity updateHardware(Long hardwareId, HardwareEntity hardwareEntity) {
+        HardwareEntity newEntity = persistence.update(hardwareEntity);
+        return newEntity;
     }
  
 }
--- s2_sitiosweb-back/src/test/java/co/edu/uniandes/csw/sitiosweb/test/logic/HardwareLogicTest.java
+++ s2_sitiosweb-back/src/test/java/co/edu/uniandes/csw/sitiosweb/test/logic/HardwareLogicTest.java
@@ -9,12 +9,18 @@ import co.edu.uniandes.csw.sitiosweb.ejb.HardwareLogic;
 import co.edu.uniandes.csw.sitiosweb.entities.HardwareEntity;
 import co.edu.uniandes.csw.sitiosweb.exceptions.BusinessLogicException;
 import co.edu.uniandes.csw.sitiosweb.persistence.HardwarePersistence;
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import uk.co.jemos.podam.api.PodamFactory;
@@ -26,6 +32,7 @@ import uk.co.jemos.podam.api.PodamFactoryImpl;
  */
 @RunWith(Arquillian.class)
 public class HardwareLogicTest {
+    
     @Deployment
     public static JavaArchive createDployment(){
         return ShrinkWrap.create(JavaArchive.class)
@@ -35,12 +42,59 @@ public class HardwareLogicTest {
                 .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
                 .addAsManifestResource("META-INF/beans.xml", "beans.xml" );
     }
-    
+
     private PodamFactory factory = new PodamFactoryImpl();
     
     @Inject
     private HardwareLogic hardwareLogic;
-    
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Inject
+    private UserTransaction utx;
+
+    private List<HardwareEntity> data = new ArrayList<>();
+
+        /**
+     * Configuración inicial de la prueba.
+     */
+    @Before
+    public void configTest() {
+        try {
+            utx.begin();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Limpia las tablas que están implicadas en la prueba.
+     */
+    private void clearData() {
+        em.createQuery("delete from HardwareEntity").executeUpdate();
+    }
+
+    /**
+     * Inserta los datos iniciales para el correcto funcionamiento de las
+     * pruebas.
+     */
+    private void insertData() {
+        for (int i = 0; i < 3; i++) {
+            HardwareEntity entity = factory.manufacturePojo(HardwareEntity.class);
+            em.persist(entity);
+            data.add(entity);
+        }
+    }
+
     @Test
     public void createHardwareTest() throws BusinessLogicException{
         HardwareEntity newEntity = factory.manufacturePojo(HardwareEntity.class);
@@ -54,5 +108,84 @@ public class HardwareLogicTest {
         newEntity.setIp(null);
         HardwareEntity result = hardwareLogic.createHardware(newEntity);
     }
+    
+    @Test(expected = BusinessLogicException.class)
+    public void createHardwareCoresZeroTest() throws BusinessLogicException{
+        HardwareEntity newEntity = factory.manufacturePojo(HardwareEntity.class);
+        newEntity.setCores(0);
+        HardwareEntity result = hardwareLogic.createHardware(newEntity);
+    }
+    
+    @Test(expected = BusinessLogicException.class)
+    public void createHardwareRamZeroTest() throws BusinessLogicException{
+        HardwareEntity newEntity = factory.manufacturePojo(HardwareEntity.class);
+        newEntity.setRam(0);
+        HardwareEntity result = hardwareLogic.createHardware(newEntity);
+    }
+    
+    @Test(expected = BusinessLogicException.class)
+    public void createHardwareCpuNullTest() throws BusinessLogicException{
+        HardwareEntity newEntity = factory.manufacturePojo(HardwareEntity.class);
+        newEntity.setCpu(null);
+        HardwareEntity result = hardwareLogic.createHardware(newEntity);
+    }
+    
+    @Test(expected = BusinessLogicException.class)
+    public void createHardwarePlataformaNullTest() throws BusinessLogicException{
+        HardwareEntity newEntity = factory.manufacturePojo(HardwareEntity.class);
+        newEntity.setPlataforma(null);
+        HardwareEntity result = hardwareLogic.createHardware(newEntity);
+    }
+    
+ 
+    @Test
+    public void getHardwaresTest() {
+        List<HardwareEntity> list = hardwareLogic.getHardwares();
+        Assert.assertEquals(data.size(), list.size());
+        for (HardwareEntity entity : list) {
+            boolean found = false;
+            for (HardwareEntity storedEntity : data) {
+                if (entity.getId().equals(storedEntity.getId())) {
+                    found = true;
+                }
+            }
+            Assert.assertTrue(found);
+        }
+    }
+
+
+    @Test
+    public void getHardwareTest() {
+        HardwareEntity entity = data.get(0);
+        HardwareEntity resultEntity = hardwareLogic.getHardware(entity.getId());
+        Assert.assertNotNull(resultEntity);
+        Assert.assertEquals(entity.getId(), resultEntity.getId());
+        Assert.assertEquals(entity.getCpu(), resultEntity.getCpu());
+        Assert.assertEquals(entity.getRam(), resultEntity.getRam());
+        Assert.assertEquals(entity.getPlataforma(), resultEntity.getPlataforma());
+        Assert.assertEquals(entity.getCores(), resultEntity.getCores());
+
+                
+    }
+
+  
+//    @Test
+//    public void updateHardwareTest() {
+//        HardwareEntity entity = data.get(0);
+//        HardwareEntity pojoEntity = factory.manufacturePojo(HardwareEntity.class);
+//
+//        pojoEntity.setId(entity.getId());
+//
+//        hardwareLogic.updateHardware(pojoEntity.getId(), pojoEntity);
+//
+//        HardwareEntity resp = em.find(HardwareEntity.class, entity.getId());
+//
+//        Assert.assertEquals(pojoEntity.getId(), resp.getId());
+//        Assert.assertEquals(entity.getId(), resp.getId());
+//        Assert.assertEquals(entity.getCpu(), resp.getCpu());
+//        Assert.assertEquals(entity.getRam(), resp.getRam());
+//        Assert.assertEquals(entity.getPlataforma(), resp.getPlataforma());
+//        Assert.assertEquals(entity.getCores(), resp.getCores());
+//    }
 
 }
