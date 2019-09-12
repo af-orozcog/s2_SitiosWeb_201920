bd334a4ffdecb8f1f008f52472095e1ed50b8c9c
Andres Orozco <af.orozcog@uniandes.edu.co>
2019-09-12 10:20:29 -0500

    se agrega  a la capa de logica

--- s2_sitiosweb-back/src/main/java/co/edu/uniandes/csw/sitiosweb/ejb/IterationLogic.java
+++ s2_sitiosweb-back/src/main/java/co/edu/uniandes/csw/sitiosweb/ejb/IterationLogic.java
@@ -8,6 +8,9 @@ package co.edu.uniandes.csw.sitiosweb.ejb;
 import co.edu.uniandes.csw.sitiosweb.entities.IterationEntity;
 import co.edu.uniandes.csw.sitiosweb.exceptions.BusinessLogicException;
 import co.edu.uniandes.csw.sitiosweb.persistence.IterationPersistence;
+import static com.sun.xml.internal.ws.spi.db.BindingContextFactory.LOGGER;
+import java.util.List;
+import java.util.logging.Level;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
@@ -33,5 +36,63 @@ public class IterationLogic {
             throw new BusinessLogicException("la fecha de validación esta vacia");
         iteracion = persistence.create(iteracion);
         return iteracion;
+    }
+    
+    /**
+     * Obtiene la lista de los registros de iterations.
+     *
+     * @return Colección de objetos de IterationEntity.
+     */
+    public List<IterationEntity> getIterations() {
+        LOGGER.log(Level.INFO, "Inicia proceso de consultar todos los autores");
+        List<IterationEntity> lista = persistence.findAll();
+        LOGGER.log(Level.INFO, "Termina proceso de consultar todos los autores");
+        return lista;
+    }
+    
+    /**
+     * Obtiene los datos de una instancia de Author a partir de su ID.
+     *
+     * @param iterationId identificador de la iteración
+     * @return Instancia de AuthorEntity con los datos del Author consultado.
+     */
+    public IterationEntity getIteration(Long iterationId) {
+        LOGGER.log(Level.INFO, "Inicia proceso de consultar el autor con id = {0}", iterationId);
+        IterationEntity iterationEntity = persistence.find(iterationId);
+        if (iterationEntity == null) {
+            LOGGER.log(Level.SEVERE, "La editorial con el id = {0} no existe", iterationId);
+        }
+        LOGGER.log(Level.INFO, "Termina proceso de consultar el autor con id = {0}", iterationId);
+        return iterationEntity;
+    }
+    
+    /**
+     * Actualiza la información de una instancia de Author.
+     *
+     * @param authorsId Identificador de la instancia a actualizar
+     * @param authorEntity Instancia de AuthorEntity con los nuevos datos.
+     * @return Instancia de AuthorEntity con los datos actualizados.
+     */
+    public IterationEntity updateIteration(Long iterionsId, IterationEntity iterationEntity) {
+        LOGGER.log(Level.INFO, "Inicia proceso de actualizar el autor con id = {0}", iterionsId);
+        IterationEntity newIterationEntity = persistence.update(iterationEntity);
+        LOGGER.log(Level.INFO, "Termina proceso de actualizar el autor con id = {0}", iterionsId);
+        return newIterationEntity;
+    }
+   
+    /**
+     * Elimina una instancia de Author de la base de datos.
+     *
+     * @param authorsId Identificador de la instancia a eliminar.
+     * @throws BusinessLogicException si el autor tiene libros asociados.
+     */
+    public void deleteIteration(Long iterationsId) throws BusinessLogicException {
+        LOGGER.log(Level.INFO, "Inicia proceso de borrar el autor con id = {0}", iterationsId);
+        IterationEntity iteration = getIteration(iterationsId);
+        if (iteration != null) {
+            throw new BusinessLogicException("No se puede borrar el autor con id = " + iterationsId + " porque tiene books asociados");
+        }
+        persistence.delete(iterationsId);
+        LOGGER.log(Level.INFO, "Termina proceso de borrar el autor con id = {0}", iterationsId);
     }
 }
