c9a9abc005fda1b8e67199414c26bf5a0e1cfa09
Daniel Galindo Ruiz <d.galindo@uniandes.edu.co>
2019-09-19 22:01:17 -0500

    Agregado ProjectResource, ProjectDTO, ProjectDetailDTO básicos

---
+++ s2_sitiosweb-api/src/main/java/co/edu/uniandes/csw/sitiosweb/dtos/ProjectDTO.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.sitiosweb.dtos;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Daniel Galindo Ruiz
+ */
+public class ProjectDTO implements Serializable{
+    
+    private Boolean internalProject;
+    private String company;
+
+    
+    public ProjectDTO (){
+        
+    }
+    /**
+     * @return the internalProject
+     */
+    public Boolean getInternalProject() {
+        return internalProject;
+    }
+
+    /**
+     * @param internalProject the internalProject to set
+     */
+    public void setInternalProject(Boolean internalProject) {
+        this.internalProject = internalProject;
+    }
+
+    /**
+     * @return the company
+     */
+    public String getCompany() {
+        return company;
+    }
+
+    /**
+     * @param company the company to set
+     */
+    public void setCompany(String company) {
+        this.company = company;
+    }
+    
+}
