6d0ca266046cb2bb6c9401452611988e63ebd1f8
Andres Orozco <af.orozcog@uniandes.edu.co>
2019-09-23 10:42:50 -0500

    recurso Iteration(vacio)

--- s2_sitiosweb-api/src/main/java/co/edu/uniandes/csw/sitiosweb/dtos/IterationDTO.java
+++ s2_sitiosweb-api/src/main/java/co/edu/uniandes/csw/sitiosweb/dtos/IterationDTO.java
@@ -11,7 +11,7 @@ import java.io.Serializable;
  *
  * @author Andres orozco 201730058
  */
-class IterationDTO implements Serializable {
+public class IterationDTO implements Serializable {
        private Long id;
        private String name;
 
--- s2_sitiosweb-api/src/main/java/co/edu/uniandes/csw/sitiosweb/resources/IterationResource.java
+++ s2_sitiosweb-api/src/main/java/co/edu/uniandes/csw/sitiosweb/resources/IterationResource.java
@@ -12,15 +12,29 @@ package co.edu.uniandes.csw.sitiosweb.resources;
 
 
 import java.io.Serializable;
+import java.util.logging.Logger;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Path;
 import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.POST;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
+import co.edu.uniandes.csw.sitiosweb.dtos.IterationDTO;
 
-
+@Path("iterations")
+@Produces("application/json")
+@Consumes("application/json")
+@RequestScoped
 public class IterationResource implements Serializable {
+    
+    private static final Logger LOGGER = Logger.getLogger(IterationResource.class.getName());
+    
+    
+    @POST
+    public IterationDTO createIteration(IterationDTO iteration){
+        
+    }
     
     @Override
     public String toString(){
