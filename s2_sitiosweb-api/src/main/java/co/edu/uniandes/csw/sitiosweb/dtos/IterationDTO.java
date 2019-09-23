bd747b1adba20d9f01a6d891ed5ac16104adca15
Andres Orozco <af.orozcog@uniandes.edu.co>
2019-09-23 12:28:58 -0500

    se termina el iterationResource

--- s2_sitiosweb-api/src/main/java/co/edu/uniandes/csw/sitiosweb/dtos/IterationDTO.java
+++ s2_sitiosweb-api/src/main/java/co/edu/uniandes/csw/sitiosweb/dtos/IterationDTO.java
@@ -5,20 +5,120 @@
  */
 package co.edu.uniandes.csw.sitiosweb.dtos;
 
+import co.edu.uniandes.csw.sitiosweb.entities.IterationEntity;
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  *
  * @author Andres orozco 201730058
  */
 public class IterationDTO implements Serializable {
-       private Long id;
-       private String name;
-
+    
+    
+    private String objetive;
+    private Date validationDate;
+    private String changes;
+    private Date beginDate;
+    private Date endDate;
+    private Long id;
        
     public IterationDTO(){
     
-       }
+    }
+    
+    public IterationDTO(IterationEntity iterationEntity) {
+        if (iterationEntity != null) {
+            this.id = iterationEntity.getId();
+            this.objetive = iterationEntity.getObjetive();
+            this.validationDate = iterationEntity.getValidationDate();
+            this.changes = iterationEntity.getChanges();
+            this.beginDate = iterationEntity.getBeginDate();
+            this.endDate = iterationEntity.getEndDate();
+        }
+    }
+
+    public IterationEntity toEntity() {
+        IterationEntity iterationEntity = new IterationEntity();
+        iterationEntity.setId(this.getId());
+        iterationEntity.setObjetive(this.getObjetive());
+        iterationEntity.setValidationDate(this.getValidationDate());
+        iterationEntity.setChanges(this.getChanges());
+        iterationEntity.setBeginDate(this.getBeginDate());
+        iterationEntity.setEndDate(this.getEndDate());
+        return iterationEntity;
+    }
+    
+    /**
+     * @return the objetive
+     */
+    public String getObjetive() {
+        return objetive;
+    }
+
+    /**
+     * @param objetive the objetive to set
+     */
+    public void setObjetive(String objetive) {
+        this.objetive = objetive;
+    }
+
+    /**
+     * @return the validationDate
+     */
+    public Date getValidationDate() {
+        return validationDate;
+    }
+
+    /**
+     * @param validationDate the validationDate to set
+     */
+    public void setValidationDate(Date validationDate) {
+        this.validationDate = validationDate;
+    }
+
+    /**
+     * @return the changes
+     */
+    public String getChanges() {
+        return changes;
+    }
+
+    /**
+     * @param changes the changes to set
+     */
+    public void setChanges(String changes) {
+        this.changes = changes;
+    }
+
+    /**
+     * @return the beginDate
+     */
+    public Date getBeginDate() {
+        return beginDate;
+    }
+
+    /**
+     * @param beginDate the beginDate to set
+     */
+    public void setBeginDate(Date beginDate) {
+        this.beginDate = beginDate;
+    }
+
+    /**
+     * @return the endDate
+     */
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    /**
+     * @param endDate the endDate to set
+     */
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
     /**
      * @return the id
      */
@@ -31,20 +131,6 @@ public class IterationDTO implements Serializable {
      */
     public void setId(Long id) {
         this.id = id;
-    }
-
-    /**
-     * @return the name
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * @param name the name to set
-     */
-    public void setName(String name) {
-        this.name = name;
     }
        
 }
