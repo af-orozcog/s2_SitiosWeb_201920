187afe0d0833b460675f658edbd1541e99f6c2d8
nf.abondano <nf.abondano@uniandes.edu.co>
2019-09-24 10:22:47 -0500

    Solución

--- s2_sitiosweb-back/src/main/java/co/edu/uniandes/csw/sitiosweb/entities/RequestEntity.java
+++ s2_sitiosweb-back/src/main/java/co/edu/uniandes/csw/sitiosweb/entities/RequestEntity.java
@@ -9,8 +9,11 @@ import co.edu.uniandes.csw.sitiosweb.podam.DateStrategy;
 import java.io.Serializable;
 import java.util.Date;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import uk.co.jemos.podam.common.PodamExclude;
 import uk.co.jemos.podam.common.PodamIntValue;
 import uk.co.jemos.podam.common.PodamStrategyValue;
 import uk.co.jemos.podam.common.PodamStringValue;
@@ -23,6 +26,10 @@ import uk.co.jemos.podam.common.PodamStringValue;
 public class RequestEntity extends BaseEntity implements Serializable
 {
     // Attributes
+    
+    @PodamExclude
+    @ManyToOne
+    private RequesterEntity requester;
     
     /**
      * Name of the request.
--- s2_sitiosweb-back/src/main/java/co/edu/uniandes/csw/sitiosweb/entities/UnitEntity.java
+++ s2_sitiosweb-back/src/main/java/co/edu/uniandes/csw/sitiosweb/entities/UnitEntity.java
@@ -7,6 +7,8 @@ package co.edu.uniandes.csw.sitiosweb.entities;
 
 import java.io.Serializable;
 import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import uk.co.jemos.podam.common.PodamExclude;
 import uk.co.jemos.podam.common.PodamStringValue;
 
 /**
@@ -16,6 +18,10 @@ import uk.co.jemos.podam.common.PodamStringValue;
 public class UnitEntity extends BaseEntity implements Serializable
 {
     // Attributes
+ 
+    @PodamExclude
+    @OneToOne (mappedBy = "unit")
+    private UnitEntity unit;
     
     /**
      * Name of the unit.
