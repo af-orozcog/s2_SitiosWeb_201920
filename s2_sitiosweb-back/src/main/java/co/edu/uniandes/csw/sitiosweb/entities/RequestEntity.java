cb78297c700536a08c60581bcaf0d75b767e1a20
Daniel del Castillo <d.delcastillo@uniandes.edu.co>
2019-10-02 13:30:05 -0500

    Auto stash before merge of "master" and "origin/master"

--- s2_sitiosweb-back/src/main/java/co/edu/uniandes/csw/sitiosweb/entities/RequestEntity.java
+++ s2_sitiosweb-back/src/main/java/co/edu/uniandes/csw/sitiosweb/entities/RequestEntity.java
@@ -1,4 +1,4 @@
-/*
+ /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -9,8 +9,9 @@ import co.edu.uniandes.csw.sitiosweb.podam.DateStrategy;
 import java.io.Serializable;
 import java.util.Date;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import uk.co.jemos.podam.common.PodamExclude;
@@ -20,12 +21,67 @@ import uk.co.jemos.podam.common.PodamStringValue;
 
 /**
  *
- * @author Daniel del Castillo
+ * @author Daniel del Castillo A.
  */
 @Entity
 public class RequestEntity extends BaseEntity implements Serializable
 {
+    // Enums
+    
+    /**
+     * Enumeration of the request's RequestType.
+     */
+    public enum RequestType
+    {
+        Elimination,
+        Creation,
+        Change
+    }
+    
+    /**
+     * Enumeration of the request's WebCategory.
+     */
+    public enum WebCategory
+    {
+        Descriptive,
+        Aplication,
+        Event
+    }
+    
+    /**
+     * Enumeration of the request's Status.
+     */
+    public enum Status
+    {
+        Development,
+        Production,
+        Accepted,
+        Pending,
+        Denied
+    }
+    
     // Attributes
+    
+    /**
+     * The request's status.
+     */
+    @PodamExclude
+    @Enumerated(EnumType.STRING)
+    private Status status;
+    
+    /**
+     * The request's web category.
+     */
+    @PodamExclude
+    @Enumerated(EnumType.STRING)
+    private WebCategory webCategory;
+    
+    /**
+     * The request's type.
+     */
+    @PodamExclude
+    @Enumerated(EnumType.STRING)
+    private RequestType requestType;
     
     /**
      * Relationship where a request has one requester.
@@ -91,119 +147,163 @@ public class RequestEntity extends BaseEntity implements Serializable
     @Temporal(TemporalType.DATE)
     @PodamStrategyValue(DateStrategy.class)
     private Date endDate;
-    
-    // TODO Status, Category, Type & Requester.
 
     // Methods
     
     /**
-     * @return the name
+     * @return the name.
      */
     public String getName() 
     { return name; }
 
     /**
-     * @param name the name to set
+     * @param name the name to set.
      */
     public void setName(String name) 
     { this.name = name; }
 
     /**
-     * @return the purpose
+     * @return the purpose.
      */
     public String getPurpose() 
     { return purpose; }
 
     /**
-     * @param purpose the purpose to set
+     * @param purpose the purpose to set.
      */
     public void setPurpose(String purpose) 
     { this.purpose = purpose; }
 
     /**
-     * @return the description
+     * @return the description.
      */
     public String getDescription() 
     { return description; }
 
     /**
-     * @param description the description to set
+     * @param description the description to set.
      */
     public void setDescription(String description) 
     { this.description = description; }
 
     /**
-     * @return the unit
+     * @return the unit.
      */
     public String getUnit() 
     { return unit; }
 
     /**
-     * @param unit the unit to set
+     * @param unit the unit to set.
      */
     public void setUnit(String unit) 
     { this.unit = unit; }
 
     /**
-     * @return the budget
+     * @return the budget.
      */
     public Integer getBudget() 
     { return budget; }
 
     /**
-     * @param budget the budget to set
+     * @param budget the budget to set.
      */
     public void setBudget(Integer budget) 
     { this.budget = budget; }
 
     /**
-     * @return the beginDate
+     * @return the beginDate.
      */
     public Date getBeginDate() 
     { return beginDate; }
 
     /**
-     * @param beginDate the beginDate to set
+     * @param beginDate the beginDate to set.
      */
     public void setBeginDate(Date beginDate) 
     { this.beginDate = beginDate; }
 
     /**
-     * @return the dueDate
+     * @return the dueDate.
      */
     public Date getDueDate() 
     { return dueDate; }
 
     /**
-     * @param dueDate the dueDate to set
+     * @param dueDate the dueDate to set.
      */
     public void setDueDate(Date dueDate) 
     { this.dueDate = dueDate; }
 
     /**
-     * @return the endDate
+     * @return the endDate.
      */
     public Date getEndDate() 
     { return endDate; }
 
     /**
-     * @param endDate the endDate to set
+     * @param endDate the endDate to set.
      */
     public void setEndDate(Date endDate) 
     { this.endDate = endDate; }
     
     
     /**
-     * @return the requester
+     * @return the requester.
      */
-    public RequesterEntity getRequester() {
-        return requester;
-    }
+    public RequesterEntity getRequester() 
+    { return requester; }
 
     /**
-     * @param requester the requester to set
+     * @param requester the requester to set.
      */
-    public void setRequester(RequesterEntity requester) {
-        this.requester = requester;
-    }
+    public void setRequester(RequesterEntity requester) 
+    { this.requester = requester; }
+
+    /**
+     * @return the project.
+     */
+    public ProjectEntity getProject() 
+    { return project; }
+
+    /**
+     * @param project the project to set.
+     */
+    public void setProject(ProjectEntity project) 
+    { this.project = project; }
+
+    /**
+     * @return the status.
+     */
+    public Status getStatus() 
+    { return status; }
+
+    /**
+     * @param status the status to set.
+     */
+    public void setStatus(Status status) 
+    { this.status = status; }
+
+    /**
+     * @return the webCategory.
+     */
+    public WebCategory getWebCategory() 
+    { return webCategory; }
+
+    /**
+     * @param webCategory the webCategory to set.
+     */
+    public void setWebCategory(WebCategory webCategory) 
+    { this.webCategory = webCategory; }
+
+    /**
+     * @return the requestType.
+     */
+    public RequestType getRequestType() 
+    { return requestType; }
+
+    /**
+     * @param requestType the requestType to set.
+     */
+    public void setRequestType(RequestType requestType) 
+    { this.requestType = requestType; }
 }
