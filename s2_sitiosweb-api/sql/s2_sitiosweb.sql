851fa825e369f4642dfe7e5fa166acad343bf6ba
Daniel Galindo Ruiz <d.galindo@uniandes.edu.co>
2019-10-09 09:03:53 -0500

    sql nada

---
+++ s2_sitiosweb-api/sql/s2_sitiosweb.sql
@@ -0,0 +1,8 @@
+delete from requesterentity;
+
+insert into requesterentity (id, login, email, phone) values (1, 'j.cano', 'j.cano@gmail.com', '124578');
+insert into requesterentity (id, login, email, phone) values (2, 'l.herrera', 'lherre@gmail.com', '528417');
+insert into requesterentity (id, login, email, phone) values (3, 'n.guevara', 'nguev@gmail.com', '658124');
+insert into requesterentity (id, login, email, phone) values (4, 'd.giraldo', 'dgir@gmail.com', '98736');
+
+select * from requesterentity;
