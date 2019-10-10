edc68a2f13da98e6278c2f52e98bdbc936dd2cf8
nf.abondano <nf.abondano@uniandes.edu.co>
2019-10-09 19:48:34 -0500

    Relacion ProjectDeveloper

--- s2_sitiosweb-api/src/test/java/co/edu/uniandes/csw/sitiosweb/tests/postman/DeveloperIT.java
+++ s2_sitiosweb-api/src/test/java/co/edu/uniandes/csw/sitiosweb/tests/postman/DeveloperIT.java
@@ -6,9 +6,9 @@
 package co.edu.uniandes.csw.sitiosweb.tests.postman;
 
 import co.edu.uniandes.csw.postman.tests.PostmanTestBuilder;
-import co.edu.uniandes.csw.sitiosweb.dtos.HardwareDTO;
+import co.edu.uniandes.csw.sitiosweb.dtos.DeveloperDTO;
 import co.edu.uniandes.csw.sitiosweb.mappers.BusinessLogicExceptionMapper;
-import co.edu.uniandes.csw.sitiosweb.resources.HardwareResource;
+import co.edu.uniandes.csw.sitiosweb.resources.DeveloperResource;
 import java.io.File;
 import java.io.IOException;
 import org.jboss.arquillian.container.test.api.Deployment;
@@ -26,9 +26,9 @@ import org.junit.runner.RunWith;
  * @author s.santosb
  */
 @RunWith(Arquillian.class)
-public class HardwareIT {
+public class DeveloperIT {
     
-    private static final String COLLECTION = "Hardware-Tests.postman_collection";
+    private static final String COLLECTION = "Developer-Tests.postman_collection";
 
     @Deployment(testable = true)
     public static WebArchive createDeployment() {
@@ -38,8 +38,8 @@ public class HardwareIT {
                         .importRuntimeDependencies().resolve()
                         .withTransitivity().asFile())
                 // Se agregan los compilados de los paquetes de servicios
-                .addPackage(HardwareResource.class.getPackage()) //No importa cual recurso usar, lo importante es agregar el paquet
-                .addPackage(HardwareDTO.class.getPackage()) //No importa cual dto usar, lo importante es agregar el paquete.
+                .addPackage(DeveloperResource.class.getPackage()) //No importa cual recurso usar, lo importante es agregar el paquet
+                .addPackage(DeveloperDTO.class.getPackage()) //No importa cual dto usar, lo importante es agregar el paquete.
                 .addPackage(BusinessLogicExceptionMapper.class.getPackage())
                 // El archivo que contiene la configuracion a la base de datos.
                 .addAsResource("META-INF/persistence.xml", "META-INF/persistence.xml")
