128a2c196df3a9f3e9b18167c92e40ed4c75cf27
Andres Martinez <a.martinez@uniandes.edu.co>
2019-10-10 00:35:38 -0500

    provider DTOs y Rrsource

---
+++ s2_sitiosweb-api/src/main/java/co/edu/uniandes/csw/sitiosweb/dtos/ProviderDetailDTO.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.sitiosweb.dtos;
+
+import co.edu.uniandes.csw.sitiosweb.entities.ProjectEntity;
+import co.edu.uniandes.csw.sitiosweb.entities.ProviderEntity;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Andres Martinez Silva
+ */
+public class ProviderDetailDTO extends ProviderDTO implements Serializable{
+    
+    private List<ProjectDTO> projects;
+    
+    public ProviderDetailDTO(){
+        super();
+    }
+    
+    public ProviderDetailDTO(ProviderEntity providerEntity){
+        super(providerEntity);
+        if(providerEntity.getProjects() != null){
+            projects = new ArrayList<>();
+            for(ProjectEntity entityProject: providerEntity.getProjects()){
+                projects.add(new ProjectDTO(entityProject));
+            }
+        }
+    }
+    
+    @Override
+    public ProviderEntity toEntity(){
+        ProviderEntity providerEntity= super.toEntity();
+         if(getProjects() != null){
+             ArrayList<ProjectEntity> projectsEntity = new ArrayList<>();
+             for(ProjectDTO dtoProject: getProjects()){
+                projectsEntity.add(dtoProject.toEntity());
+            }
+             providerEntity.setProjects(projectsEntity);
+         }
+         return providerEntity;
+    }
+
+    public List<ProjectDTO> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(List<ProjectDTO> projects) {
+        this.projects = projects;
+    }
+    
+    
+    
+}
