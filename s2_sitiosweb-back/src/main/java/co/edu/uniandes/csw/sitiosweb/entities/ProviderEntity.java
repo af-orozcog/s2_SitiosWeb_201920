38074fb5a1fca4bf70defc991ce79340811e6552
Andres Martinez <a.martinez@uniandes.edu.co>
2019-10-09 22:19:06 -0500

    providerBaseFull

--- s2_sitiosweb-back/src/main/java/co/edu/uniandes/csw/sitiosweb/entities/ProviderEntity.java
+++ s2_sitiosweb-back/src/main/java/co/edu/uniandes/csw/sitiosweb/entities/ProviderEntity.java
@@ -1,13 +1,14 @@
 
 
+
 package co.edu.uniandes.csw.sitiosweb.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.CascadeType;
 import uk.co.jemos.podam.common.PodamExclude;
 /**
  * @author Andres Martinez Silva 
@@ -17,10 +18,6 @@ import uk.co.jemos.podam.common.PodamExclude;
 public class ProviderEntity extends BaseEntity implements Serializable{
 
     private String name;
-
-    @PodamExclude
-    @OneToOne
-    private ProjectEntity project;
     
     public String getName() {
         return name;
@@ -29,5 +26,18 @@ public class ProviderEntity extends BaseEntity implements Serializable{
     public void setName(String name) {
         this.name = name;
     }
+    
+    @PodamExclude
+    @OneToMany(mappedBy = "provider", cascade = CascadeType.PERSIST, orphanRemoval = false)
+    private List<ProjectEntity> projects = new ArrayList<ProjectEntity>();
+    
+    public List<ProjectEntity> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(List<ProjectEntity> projects) {
+        this.projects = projects;
+    }
+
     
 }
--- s2_sitiosweb-back/src/main/java/co/edu/uniandes/csw/sitiosweb/persistence/ProviderPersistence.java
+++ s2_sitiosweb-back/src/main/java/co/edu/uniandes/csw/sitiosweb/persistence/ProviderPersistence.java
@@ -6,7 +6,6 @@
 package co.edu.uniandes.csw.sitiosweb.persistence;
 
 import co.edu.uniandes.csw.sitiosweb.entities.ProviderEntity;
-import co.edu.uniandes.csw.sitiosweb.entities.RequestEntity;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -42,21 +41,58 @@ public class ProviderPersistence {
         return query.getResultList();
     }
     
-        public ProviderEntity find(Long authorsId) {
-        LOGGER.log(Level.INFO, "Consultando el proveedor con id={0}", authorsId);
-
-        return em.find(ProviderEntity.class, authorsId);
+        public ProviderEntity find(Long providersId) {
+        LOGGER.log(Level.INFO, "Consultando el proveedor con id={0}", providersId);
+        ProviderEntity provider = em.find(ProviderEntity.class, providersId);
+        return provider;
     }
         
-        public ProviderEntity update(ProviderEntity authorEntity) {
-        LOGGER.log(Level.INFO, "Actualizando el proveedor con id={0}", authorEntity.getId());
-        return em.merge(authorEntity);
+
+        public ProviderEntity findById( Long providersId) {
+       LOGGER.log(Level.INFO, "Consultando proveedor por Id ", providersId);
+        TypedQuery query = em.createQuery("Select e From ProviderEntity e where e.isbn = :id", ProviderEntity.class);
+        query = query.setParameter("id", providersId);
+        List<ProviderEntity> sameId = query.getResultList();
+        ProviderEntity result;
+        if (sameId == null) {
+            result = null;
+        } else if (sameId.isEmpty()) {
+            result = null;
+        } else {
+            result = sameId.get(0);
+        }
+        LOGGER.log(Level.INFO, "Saliendo de consultar proveedores por id ", providersId);
+        return result;
+    }   
+        
+        public ProviderEntity findByName( String providersName) {
+       LOGGER.log(Level.INFO, "Consultando proveedor por nombre ", providersName);
+        TypedQuery query = em.createQuery("Select e From ProviderEntity e where e.name = :nombre", ProviderEntity.class);
+        query = query.setParameter("nombre", providersName);
+        List<ProviderEntity> sameNombre = query.getResultList();
+        ProviderEntity result;
+        if (sameNombre == null) {
+            result = null;
+        } else if (sameNombre.isEmpty()) {
+            result = null;
+        } else {
+            result = sameNombre.get(0);
+        }
+        LOGGER.log(Level.INFO, "Saliendo de consultar proveedores por nombre ", providersName);
+        return result;
+    } 
+ 
+
+        
+        public ProviderEntity update(ProviderEntity providerEntity) {
+        LOGGER.log(Level.INFO, "Actualizando el proveedor con id={0}", providerEntity.getId());
+        return em.merge(providerEntity);
     }
         
-            public void delete(Long providersId) {
-
+        public void delete(Long providersId) {
         LOGGER.log(Level.INFO, "Borrando el proveedor con id={0}", providersId);
-        ProviderEntity authorEntity = em.find(ProviderEntity.class, providersId);
-        em.remove(authorEntity);
+        ProviderEntity providerEntity = em.find(ProviderEntity.class, providersId);
+        em.remove(providerEntity);
+        LOGGER.log(Level.INFO, "Saliendo de borrar El provider con id = {0}", providersId);
     }
 }
