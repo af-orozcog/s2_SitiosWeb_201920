521cf9b401cfc1c4c21d87ff28a6ca0d1a373b98
Daniel Galindo Ruiz <d.galindo@uniandes.edu.co>
2019-11-13 09:51:29 -0500

    capo sql

--- s2_sitiosweb-api/sql/s2_sitiosweb.sql
+++ s2_sitiosweb-api/sql/s2_sitiosweb.sql
@@ -2,3 +2,6 @@ DELETE FROM USERENTITY;
 DELETE FROM PROJECTENTITY;
 DELETE FROM UNITENTITY;
 select * from userentity;
+select * from projectentity;
+select * from hardwareentity;
+insert into hardwareentity (cores,cpu,ip,plataforma,ram) values(8,' Intel i5' ,1127181,'Windows 7 ' ,16);
